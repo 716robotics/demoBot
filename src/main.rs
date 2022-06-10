7f0c87a3a489dbc67fdc3b910a909a6ae919d33d
cannotilever <cannotilever@protonmail.com>
2022-06-10 19:45:22 -0400

    PWM experimentatin: It works!!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use linux_embedded_hal::I2cdev;
+use pwm_pca9685::{Address, Channel, Pca9685};
+use std::{thread, time::Duration};
 fn main() {
-    println!("Hello, world!");
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let address = Address::from(0x40);
+    let mut pwm = Pca9685::new(dev, address).unwrap();
+    // This corresponds to a frequency of 60 Hz.
+    pwm.set_prescale(122).unwrap();
+
+    // It is necessary to enable the device.
+    pwm.enable().unwrap();
+    pwm.set_channel_on_off(Channel::C15, 0,409).unwrap();
+    thread::sleep(Duration::from_millis(3000));
+    pwm.set_channel_on_off(Channel::C15, 0,307).unwrap();
+    thread::sleep(Duration::from_millis(3000));
+    pwm.set_channel_on_off(Channel::C15, 0,205).unwrap();
+    thread::sleep(Duration::from_millis(3000));
+    pwm.set_channel_on_off(Channel::C15, 0,307).unwrap();
+    thread::sleep(Duration::from_millis(3000));
+    pwm.set_channel_on_off(Channel::C15, 0,409).unwrap();
+    thread::sleep(Duration::from_millis(3000));
+    pwm.set_channel_on_off(Channel::C15, 0,307).unwrap();
+
+    let _dev = pwm.destroy(); // Get the I2C device back
 }
